5ee5c37bdb574a81a0db79dee71f9de0bb97f439
Hausel Kinga <hausel.kinga@gmail.com>
2023-06-30 09:43:34 +0200

    add helloHandler function

--- main.go
+++ main.go
@@ -7,7 +7,15 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request){
-
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request){
